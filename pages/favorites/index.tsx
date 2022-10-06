25da2950f111ddd8b11726bbe8f2a217aa45b609
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-06 19:39:00 -0300

    Favorites page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Layout } from "../../components/layouts/Layout";
+import { FavoritePokemons } from "../../components/pokemon";
+import { NoFavorites } from "../../components/ui";
+import { localFavorites } from "../../utils";
 
-const index = () => {
+const FavoritesPage = () => {
+	const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+	useEffect(() => {
+		setFavoritePokemons(localFavorites.pokemons());
+	}, []);
+
 	return (
 		<Layout title="PokemonApp | Favorites">
-			<h1>Favorites</h1>
+			{favoritePokemons.length === 0 ? (
+				<NoFavorites />
+			) : (
+				<FavoritePokemons pokemons={favoritePokemons} />
+			)}
 		</Layout>
 	);
 };
 
-export default index;
+export default FavoritesPage;
